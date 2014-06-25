0aeeac00ce6b6dc07c69e926d9f903a3f1decd0b
Jesus Magana <maganazero5@gmail.com>
2014-06-24 20:28:01 -0700

    Initial Commit

---
+++ MarcoPollo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MarcoPollo
+//
+//  Created by Jesus Magana on 6/24/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MarcoPollo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MarcoPollo
+//
+//  Created by Jesus Magana on 6/24/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
